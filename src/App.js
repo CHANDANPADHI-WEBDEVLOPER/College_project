dad55841b57110da04ae600de91a7200c8e75b3c
shashanka panda <shashanka.panda@gmail.com>
2021-12-02 23:24:20 +0530

    contact page processing

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
-
+import ContactUs from "./components/contact/Contactus";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-     
-     
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ContactUs />
     </div>
   );
 }
